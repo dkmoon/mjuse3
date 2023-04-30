d290fd06e9a6cd43e95ef7320726f9f227811000
DK Moon <dkmoon@gmail.com>
2023-05-01 01:24:10 +0900

    added test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+'''
+연습용 모듈
+
+모듈 수준에서의 코멘트 추가한 것
+'''
+
+class MyClass:
+    '''
+    연습용 클래스
+    '''
+    def method(a):
+        '''
+        메서드에 대한 설명
+        :param a: 매개변수
+        '''
+        pass
+
+def testFunction(b):
+    '''
+    docstring 연습용 함수
+    :param b: 매개변수
+    :return: 값을 그대로 반환함
+    '''
+    return b
